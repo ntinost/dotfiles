fc460c34dc07f3a7e3025263072142f46d3c539d
Konstantinos Toumpidis <konstantinos.toumpidis@protonmail.ch>
2016-06-16 21:47:25 +0300

    moar blocks

---
+++ i3wm/i3bar/vpn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VPNC_FILE='/var/run/openvpn@tux-full-udp.pid'
+
+if [[ -e "${VPNC_FILE}" ]]; then
+  echo ""
+  echo ""
+  echo ""
+else
+  echo ""
+  echo ""
+  echo \#DC322F
+fi
