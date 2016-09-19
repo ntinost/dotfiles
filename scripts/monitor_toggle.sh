91db13aa8e2ab177efd46b2f5fb554f2eef365b0
Konstantinos Toumpidis <konstantinos.toumpidis@protonmail.ch>
2016-09-20 01:23:57 +0300

    fix strange feh bg

--- scripts/monitor_toggle.sh
+++ scripts/monitor_toggle.sh
@@ -4,6 +4,8 @@ extern=VGA1
 
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
+	feh --bg-scale $DOTFILES/i3wm/wallhaven-7427.png
 else
     xrandr --output "$intern" --off --output "$extern" --auto
+	feh --bg-scale $DOTFILES/i3wm/wallhaven-7427.png
 fi
