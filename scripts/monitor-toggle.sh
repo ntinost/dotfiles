f8f98d17b269ea37aa965aedb7b7a3276cc7ec96
Konstantinos Toumpidis <ktoump@openmailbox.org>
2019-05-27 15:43:49 +0300

    changes. CHANGES!

--- scripts/monitor-toggle.sh
+++ scripts/monitor-toggle.sh
@@ -7,5 +7,5 @@ if xrandr | grep "HDMI-2 disconnected"; then
 else
 	nmcli radio wifi off
 	xmodmap ~/.Xmod_left
-	xrandr --output VGA-1 --off --output LVDS-1 --primary --mode 1366x768 --pos 1920x320 --rotate normal --output HDMI-3 --off --output HDMI-2 --mode 1920x1080 --pos 0x8 --rotate normal --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+	xrandr --output VGA-1 --off --output LVDS-1 --off --output HDMI-3 --off --output HDMI-2 --mode 1920x1080 --rotate normal --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
 fi
