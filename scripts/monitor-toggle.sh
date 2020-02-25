f3b5fde53c0821ee138789596d2332256c63ab9e
Konstantinos Toumpidis <ktoump@openmailbox.org>
2020-02-25 15:48:57 +0200

    hell

--- scripts/monitor-toggle.sh
+++ scripts/monitor-toggle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if xrandr | grep "HDMI-2 disconnected"; then
+if xrandr | grep "HDMI-3 disconnected"; then
 	nmcli radio wifi on
-	xmodmap ~/.Xmod_right
 	xrandr --output VGA-1 --off --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+	sh ~/.dotfiles/scripts/launch_polybar.sh
 else
 	nmcli radio wifi off
-	xmodmap ~/.Xmod_left
-	xrandr --output VGA-1 --off --output LVDS-1 --off --output HDMI-3 --off --output HDMI-2 --mode 1920x1080 --rotate normal --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+	xrandr --output VGA-1 --off --output LVDS-1 --off --output HDMI-2 --off --output HDMI-3 --mode 1920x1080 --rotate normal --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+	sh ~/.dotfiles/scripts/launch_polybar.sh
 fi
